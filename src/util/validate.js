4f520b1468fc6af7757173976fca741e5094d213
manNomi <manNomi@users.noreply.github.com>
2025-10-24 22:33:46 +0900

    feat: Validate 구현 isInteger , inNumber 구현

--- src/util/validate.js
+++ src/util/validate.js
@@ -1,1 +1,10 @@
-export const validate = {};
+export const validate = {
+  isNumber(input) {
+    const number = Number(input);
+    return !Number.isNaN(number);
+  },
+  isInteger(input) {
+    const number = Number(input);
+    return Number.isInteger(number);
+  },
+};
--- src/util/validate.test.js
+++ src/util/validate.test.js
@@ -7,4 +7,15 @@ describe('validate 테스트', () => {
     expect(validate.isNumber('5')).toBe(true);
     expect(validate.isNumber('abc')).toBe(false);
   });
+
+  test('isInteger 테스트', () => {
+    const isInteger = (input) => {
+      const number = Number(input);
+      return Number.isInteger(number);
+    };
+
+    expect(isInteger('5')).toBe(true);
+    expect(isInteger('5.5')).toBe(false);
+    expect(isInteger('abc')).toBe(false);
+  });
 });
